dcbb4c9e69a5f5757d041006ecc4f41505ab431a
Fralou <fralou@protonmail.com>
2023-05-04 14:30:14 +0300

    feat: solve 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const { stdout } = process;
+
+const fs = require('fs');
+const path = require('path');
+
+const text = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(text, 'utf8');
+
+readStream.pipe(stdout)
