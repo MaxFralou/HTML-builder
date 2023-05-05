6aa783346f0038e18fd78efe171f8eda82d2b36d
Fralou <fralou@protonmail.com>
2023-05-05 21:27:12 +0300

    feat: solve task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const path = require('path');
 
+const { promises: fsPromises } = fs;
+
+const copyDirPath = path.join(__dirname, 'files-copy');
+const readDirPath = path.join(__dirname, 'files');
+
+async function copyDirectory(src, dest) {
+  await fsPromises.mkdir(dest, { recursive: true });
+
+  const files = await fsPromises.readdir(src, { withFileTypes: true });
+  files.forEach(async (file) => {
+    const srcPath = path.join(src, file.name);
+    const destPath = path.join(dest, file.name);
+
+    if (file.isDirectory()) {
+      await copyDirectory(srcPath, destPath);
+    } else {
+      await fsPromises.copyFile(srcPath, destPath);
+    }
+  });
+}
+
+copyDirectory(readDirPath, copyDirPath)
+  .then(() => {
+    console.log('Копирование папки завершено успешно. Поздравляю!');
+  })
+  .catch((err) => {
+    console.error('Ой:', err);
+  });
